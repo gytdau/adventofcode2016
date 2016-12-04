cf31c0848193f1d665d0b5779d3f880f1107ea3d
Gytis Daujotas <gytdau@gmail.com>
2016-12-04 12:48:47 +0000

    Added seperateIntoArguments()
    First batch of tests now pass!

--- day4/part1.js
+++ day4/part1.js
@@ -2,7 +2,13 @@ function process(input) {
 
 }
 function seperateIntoArguments(line) {
-
+  var regex = /([a-z\-]+)-([0-9]+)\[([a-z]+)\]/
+  var matches = line.match(regex)
+  return {
+    name: matches[1].replace(/-/g, ""),
+    sectorID: matches[2],
+    checksum: matches[3]
+  }
 }
 function frequencySort(name) {
 
