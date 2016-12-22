8eb5471f2ff343bf87cc18c68ff78fe2e8115b4e
Gytis Daujotas <gytdau@gmail.com>
2016-12-22 11:43:13 +0000

    Added input.txt

--- day4/run.js
+++ day4/run.js
@@ -1,15 +1,9 @@
 var part1 = require('./part1')
 var part2 = require('./part2')
 
-
-
 var fs = require('fs');
-
 var input = fs.readFileSync('input.txt', 'utf8');
 
-console.log(
-  'Part 1 Answer:', part1.process(input))
-  console.log(
-  'Part 2 Answer Table - Pick out the one you need'
-  )
-  part2.process(input)
+console.log('Part 1 Answer:', part1.process(input))
+console.log('Part 2 Answer Table - Pick out the one you need')
+part2.process(input)
